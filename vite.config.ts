5dd2d064d3630903d8133d5c60cec9b40bbc73ad
Jordan Havard <sirj.havard@gmail.com>
2025-05-05 17:11:39 +1000

    chore: update build steps

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,17 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/plugins/com.qvirtual.screenshots/",
+  esbuild: {
+    pure: ["console.log", "console.info"],
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "./com.qvirtual.screenshots/ui",
   },
 });
